3bca3307fece16019e6a4ec3d9599d5eaa536b4c
N1kD21 <nikoladmit21@gmail.com>
2022-12-15 13:21:52 +0200

    crontab

--- bin/ngrok.sh
+++ bin/ngrok.sh
@@ -1,79 +1,27 @@
 #!/bin/bash
+# "конфиг ngrok.yml /home/$USERBOT/.config/ngrok/ngrok.yml  не создавать, потому что не работает ngrok"
 
-PATHCONFBOT="/home/$SUDO_USER/source/adminscript/conf"
-
-printVar()
-{
-
-    echo "-------------------------------"
-    echo "Cодержание переменных"
-    echo "USERBOT -> $USERBOT"
-    echo "SUDO_USER -> $SUDO_USER"
-    echo "LOGNAME -> $LOGNAME"
-    echo "HOME -> $HOME"
-    echo "PATHCONFBOT -> $PATHCONFBOT"
-    echo "HOMEADMIN -> $HOMEADMIN"
-    echo "HOMEINIT -> $HOMEINIT"
-    echo "HOMELOG -> $HOMELOG"
-    echo "PATHFLASH -> $PATHFLASH"
-    echo "-------------------------------"
-#    printenv
-    echo "-------------------------------"
-}
-
-
-loadconf()
-{
-
-    echo "Загружаем переменные"
-    #  указать путь, где лежит настройка
-    # путь временый ~/source/adminscript/conf/
-
-    if  [ -n "$SUDO_USER" ] ;
-	then
-        source $PATHCONFBOT/AdminScriptVar
-        . $HOMEADMIN/controlpath.sh
-        echo "SUDO_USER -> not null."
-	else
-	 echo "SUDO_USER -> null."
-	 echo "Не удалось подгрузить файл конфигурации!"
-	 echo "Необходимо использовать sudo -s"
-    fi
-
-
-}
-
-# загружаем конфигурационный файл с настройками
-
-
-#LOG=/home/$LOGNAME/log/ngrok.log
-
-loadconf
-#printVar
-
-LOG=$HOMELOG/ngrok.log
 
 echo "Start --------------------------------------"
 echo "[`date +%d-%m-%Y\ %H:%M:%S`]"
-echo "Зашел.     id"  >> $LOG
-id >> $LOG
-echo "LOGNAME -> $LOGNAME" >> $LOG
+echo "Зашел.     id"
+id
+echo "LOGNAME -> $LOGNAME"
+echo "HOMELOG -> $HOMELOG"
+echo "PATHKEY -> $PATHKEY"
+echo "PATH -> $PATH   "
+echo "USER -> $USER   "
 
-# "конфиг ngrok.yml /home/$USERBOT/.config/ngrok/ngrok.yml  не создавать, потому что не работает ngrok"
-
-echo "PATH -> $PATH   " >> $LOG
-echo "USER -> $USER   " >> $LOG
-
-killall -v ngrok >> $LOG
+killall -v ngrok
 
 
 if test -d $PATHKEY;
 then
 	echo "start ngrok +++++++++++++++++++++++"
-	eval `/usr/local/bin/ngrok http 8080 --log=stdout --log-level=info >> $LOG &`
-
+	#eval `/usr/local/bin/ngrok http 8080 --log=$HOMELOG/ngrok.log --log-level=info &`
+	/usr/local/bin/ngrok http 8080 --log=$HOMELOG/ngrok.log --log-level=info &
 else
-        echo "ngrok couldn't be started" >> $LOG
+        echo "ngrok couldn't be started"
 fi
 
 
---
+++ bin/runngrok.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# регулярный запуск ngrok через cron БЕЗ sudo
+
+# исправить на правильный путь
+
+PATHCONFBOTUSER="/home/$LOGNAME/source/adminscript/conf"
+#PATHCONFBOT="/home/$SUDO_USER/source/adminscript/conf"
+
+printVar()
+{
+    echo "-------------------------------"
+    echo "Cодержание переменных"
+    echo "USERBOT -> $USERBOT"
+    echo "SUDO_USER -> $SUDO_USER"
+    echo "LOGNAME -> $LOGNAME"
+    echo "HOME -> $HOME"
+    echo "PATHCONFBOT -> $PATHCONFBOT"
+    echo "HOMEADMIN -> $HOMEADMIN"
+    echo "HOMEINIT -> $HOMEINIT"
+    echo "HOMELOG -> $HOMELOG"
+    echo "PATHFLASH -> $PATHFLASH"
+    echo "PATHCONFBOTUSER -> $PATHCONFBOTUSER"
+    echo "PATHKEY -> $PATHKEY"
+    echo "-------------------------------"
+    #printenv
+    echo "-------------------------------"
+}
+
+loadconfUser()
+{
+    #echo "Загружаем переменные"
+    #  указать путь, где лежит настройка
+    # путь временый ~/source/adminscript/conf/
+    source $PATHCONFBOTUSER/AdminScriptVar
+    . $HOMEADMIN/controlpath.sh
+
+}
+# загружаем конфигурационный файл с настройками
+
+loadconfUser
+printVar >> $HOMELOG/ngrok.log 2>&1
+
+
+. $HOMEADMIN/ngrok.sh >> $HOMELOG/ngrok.log 2>&1
